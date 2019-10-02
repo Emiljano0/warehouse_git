bc94bd3e7b6ada1f4dd1fba101912b9aadf35ad4
Emiljano0 <55244305+Emiljano0@users.noreply.github.com>
2019-10-02 20:32:36 +0200

    Update models.py

--- series_episodes_app/models.py
+++ series_episodes_app/models.py
@@ -3,22 +3,19 @@ from datetime import datetime
 
 
 class Series_Episodes(models.Model):
-    series_episode_id = models.IntegerField(primary_key=True, default=0)
+    series_episode_title = models.CharField(max_length=100)
     series_episode_series_title = models.CharField(max_length=200, default='Planet Earth')
     series_episode_season = models.IntegerField(default=0)
     series_episode_number = models.IntegerField(default=0)
     series_episode_director = models.CharField(max_length=1000)
-    series_episode_synopsis = models.CharField(max_length=1000)
-    series_episode_title = models.CharField(max_length=100)
-    series_episode_poster = models.FileField()
-    series_episode_thumbnail = models.FileField(default='Planet Earth Thumbnail')
+    series_episode_description = models.CharField(max_length=1000)
     series_episode_release_date = models.DateField(("Date"), default='0000')
-    series_episode_budget = models.IntegerField()
-    series_episode_gross = models.IntegerField()
-    series_episode_genre = models.CharField(max_length=100, default='Action')
-    series_episode_format = models.CharField(max_length=100)
+    series_episode_genre = models.CharField(max_length=100)
+    series_episode_quality = models.CharField(max_length=100)
     series_episode_rating = models.FloatField()
-    series_episode_video = models.FileField(default='PlanetEarth.mp4')
+    series_episode_thumbnail = models.FileField()
+    series_episode_poster = models.FileField()
+    series_episode_video = models.FileField()
 
     def __str__(self):
         return self.series_episode_series_title + ' : ' + self.series_episode_title \
