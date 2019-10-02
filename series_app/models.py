154aeb85ffa41fd11da3658f01d947327bfd7a4f
Emiljano0 <55244305+Emiljano0@users.noreply.github.com>
2019-10-02 20:30:55 +0200

    Update models.py

--- series_app/models.py
+++ series_app/models.py
@@ -3,16 +3,14 @@ from datetime import datetime
 
 
 class Series(models.Model):
-    series_title = models.CharField(max_length=200, default='kkkk')
+    series_title = models.CharField(max_length=200)
     series_creator1 = models.CharField(max_length=100)
-    series_creator2 = models.CharField(max_length=100, default='None')
-    series_synopsis = models.CharField(max_length=1000)
-    series_poster = models.FileField()
+    series_creator2 = models.CharField(max_length=100)
+    series_description = models.CharField(max_length=1000)
     series_release_date = models.DateField(("Date"), default='0000')
-    series_budget = models.IntegerField()
-    series_gross = models.IntegerField()
-    series_genre = models.CharField(max_length=100, default='Action')
+    series_genre = models.CharField(max_length=100)
     series_rating = models.FloatField()
+    series_poster = models.FileField()
 
     def __str__(self):
         return self.series_title
