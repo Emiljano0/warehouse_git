29d355365c0c2d02f96774988af242a0532cc2d8
Emiljano0 <55244305+Emiljano0@users.noreply.github.com>
2019-10-02 20:28:15 +0200

    Update models.py

--- documentaries_episodes_app/models.py
+++ documentaries_episodes_app/models.py
@@ -2,22 +2,19 @@ from django.db import models
 from datetime import datetime
 
 class Documentaries_Episodes(models.Model):
-    documentary_episode_id = models.IntegerField(primary_key=True, default=0)
-    documentary_episode_documentaries_title = models.CharField(max_length=200, default='Planet Earth')
+    documentary_episode_title = models.CharField(max_length=100)
+    documentary_episode_documentaries_title = models.CharField(max_length=200)
     documentary_episode_season = models.IntegerField(default=0)
     documentary_episode_number = models.IntegerField(default=0)
-    documentary_episode_director = models.CharField(max_length=1000)
-    documentary_episode_synopsis = models.CharField(max_length=1000)
-    documentary_episode_title = models.CharField(max_length=100)
-    documentary_episode_poster = models.FileField()
-    documentary_episode_thumbnail = models.FileField(default='Planet Earth poster')
+    documentary_episode_creator = models.CharField(max_length=1000)
+    documentary_episode_description = models.CharField(max_length=1000)
     documentary_episode_release_date = models.DateField(("Date"), default='0000')
-    documentary_episode_budget = models.IntegerField()
-    documentary_episode_gross = models.IntegerField()
-    documentary_episode_genre = models.CharField(max_length=100, default='Action')
-    documentary_episode_format = models.CharField(max_length=100)
+    documentary_episode_genre = models.CharField(max_length=100)
+    documentary_episode_quality = models.CharField(max_length=100)
     documentary_episode_rating = models.FloatField()
-    documentary_episode_video = models.FileField(default='PlanetEarth.mp4')
+    documentary_episode_poster = models.FileField()
+    documentary_episode_thumbnail = models.FileField()
+    documentary_episode_video = models.FileField()
 
     def __str__(self):
         return self.documentary_episode_documentaries_title + ' : ' + self.documentary_episode_title \
