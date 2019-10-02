ae0fea788bc0a10d8ebb578d03c6d160bc307588
Emiljano0 <55244305+Emiljano0@users.noreply.github.com>
2019-10-02 20:24:58 +0200

    Update models.py

--- documentaries_app/models.py
+++ documentaries_app/models.py
@@ -3,16 +3,15 @@ from datetime import datetime
 
 
 class Documentaries(models.Model):
-    documentary_title = models.CharField(max_length=200, default='kkkk')
+    documentary_title = models.CharField(max_length=200)
     documentary_creator1 = models.CharField(max_length=100)
-    documentary_creator2 = models.CharField(max_length=100, default='None')
-    documentary_synopsis = models.CharField(max_length=1000)
-    documentary_poster = models.FileField()
+    documentary_creator2 = models.CharField(max_length=100)
+    documentary_description = models.CharField(max_length=1000) 
     documentary_release_date = models.DateField(("Date"), default='0000')
-    documentary_budget = models.IntegerField()
-    documentary_gross = models.IntegerField()
     documentary_genre = models.CharField(max_length=100, default='Action')
     documentary_rating = models.FloatField()
+    documentary_poster = models.FileField()
 
+    
     def __str__(self):
         return self.documentary_title
