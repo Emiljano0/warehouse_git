be9637feaee420bf353eb179ede9e4855c59a64e
Emiljano0 <55244305+Emiljano0@users.noreply.github.com>
2019-10-02 20:22:56 +0200

    Update models.py

--- movies_app/models.py
+++ movies_app/models.py
@@ -3,19 +3,20 @@ from datetime import datetime
 
 
 class Movies(models.Model):
-    movie_director = models.CharField(max_length=1000)
-    movie_synopsis = models.CharField(max_length=1000)
     movie_title = models.CharField(max_length=100)
-    movie_poster = models.FileField()
-    movie_thumbnail = models.FileField(default='Planet Earth thumbnail')
+    movie_director = models.CharField(max_length=1000)
+    movie_description = models.CharField(max_length=1000)
     movie_release_date = models.DateField(("Date"), default='0000')
     movie_budget = models.IntegerField()
     movie_gross = models.IntegerField()
     movie_genre = models.CharField(max_length=100)
     movie_quality = models.CharField(max_length=100)
     movie_rating = models.FloatField()
-    movie_video = models.FileField()
     movie_subtitles = models.FileField(default='Planet Earth Subtitles')
+    movie_thumbnail = models.FileField(default='Planet Earth thumbnail')
+    movie_poster = models.FileField()
+    movie_video = models.FileField()
+    
 
     def __str__(self):
         return self.movie_title
