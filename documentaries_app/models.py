c836069884407447282b2731cd1d58e8a437f70e
Emiljano0 <55244305+Emiljano0@users.noreply.github.com>
2019-10-02 20:25:10 +0200

    Update models.py

--- documentaries_app/models.py
+++ documentaries_app/models.py
@@ -8,7 +8,7 @@ class Documentaries(models.Model):
     documentary_creator2 = models.CharField(max_length=100)
     documentary_description = models.CharField(max_length=1000) 
     documentary_release_date = models.DateField(("Date"), default='0000')
-    documentary_genre = models.CharField(max_length=100, default='Action')
+    documentary_genre = models.CharField(max_length=100)
     documentary_rating = models.FloatField()
     documentary_poster = models.FileField()
 
